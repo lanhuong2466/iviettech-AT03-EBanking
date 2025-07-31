e52111bf630af4d1473a818358c4ebdfde0b38ff
picasso113 <huyphan.dnc@gmail.com>
2025-07-31 10:09:42 +0700

    init commit

--- src/test/java/TC08.java
+++ src/test/java/TC08.java
@@ -1,5 +1,3 @@
-package tests;
-
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
--- src/test/java/TC10.java
+++ src/test/java/TC10.java
@@ -1,5 +1,3 @@
-package tests;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
